92862e28fdde6359dd09fb79a7f9c5596b1d6a81
Marko <tomic.marko.mt@gmail.com>
2023-12-03 22:16:00 +0100

    Added abstract class for strategies

---
+++ backtester/strategy.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+class Strategy(ABC):
+
+    @abstractmethod
+    def long():
+        pass
+
+    @abstractmethod
+    def short():
+        pass
+
+    @abstractmethod
+    def close():
+        pass
